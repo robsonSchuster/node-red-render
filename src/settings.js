ff7ae9b47b23b928ae2a719e690bc80118579753
robsonSchuster <68903975+robsonSchuster@users.noreply.github.com>
2025-02-19 16:25:50 -0300

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -3,7 +3,12 @@ module.exports = {
   flowFile: '/opt/render/project/src/flows/flows.json',
   userDir: '/opt/render/project/src/flows',
   credentialSecret: "suaSenhaSegura",
-  httpStatic: '/opt/render/project/src/flows', // Pasta exposta
+  httpStatic: [
+    {
+      path: '/flows',  // URL para acessar os arquivos (ex: /flows/flows.json)
+      root: '/opt/render/project/src/flows' // Caminho absoluto no servidor
+    }
+  ],
   httpAdminRoot: '/admin', // Editor em /admin
   logging: {
     console: {
